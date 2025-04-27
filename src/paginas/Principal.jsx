80d8c6b44b399c5a9934b331f26a0500bdffaf7f
edu.vilacha <edu.vilacha@hotmail.com>
2025-04-27 19:06:23 +0200

    principal bien

--- src/paginas/Principal.jsx
+++ src/paginas/Principal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../img/sync_logo.png";
 import imgPerfil from "../img/sync_perfil.png";
 import imgTest from "../img/sync_test.png";
