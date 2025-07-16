d9a041b6d96734becfe6dfe0e9c46c19b369bc45
edu.vilacha <edu.vilacha@hotmail.com>
2025-07-16 18:38:54 +0200

    botón

--- src/paginas/Test.jsx
+++ src/paginas/Test.jsx
@@ -88,12 +88,11 @@ const Test = () => {
       
       {/* Botón cerrar sesión */}
       <button
-        style={{ position: "absolute", top: "25px", right: "50px" }}
-        className="py-[10px] px-[20px] text-[#ffffff] bg-[#ff2d01] hover:bg-[#ff78e5] border-none transition z-50"
-        onClick={handleLogout}
-      >
-        Cerrar sesión
-      </button>
+          onClick={handleLogout}
+          className="absolute top-5 right-10 py-2 px-4 text-white bg-[#ff2d01] hover:bg-[#ff78e5] rounded"
+        >
+          Cerrar sesión
+        </button>
 
       {/* Logo */}
       <div className="w-full flex justify-start ml-6 mt-4 z-10 relative">
